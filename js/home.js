d427938f7d2f921976817650d9ae0653fdd8ff80
xujianhua <xujianhua@outlook.com>
2016-07-30 00:02:54 +0800

    项目信息tab页面内容更新，项目照片页面完成。

--- js/home.js
+++ js/home.js
@@ -358,3 +358,29 @@ $(document).on("click", ".projectInfoPopup  .selectWhereRows>.selectBtn>.add", f
 $(".rightArea  .markingRecords").click(function () {
     $.layer({ title: "扣分记录", url: "markingRecordsPopup.html" });
 });
+
+/* 编辑项目图片文本*/
+$("body > .centerArea > .projectContent .contentArea .contentAreaEdit input").click(function () {
+    $.layer({ title: "编辑项目图片文本", url: "contentAreaEditPopup.html" });
+});
+
+//图片插件
+$(document).ready(function () {
+    $('.centerArea>.projectContent>.rows>.content>.photo').magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        tLoading: 'Loading image #%curr%...',
+        mainClass: 'mfp-img-mobile',
+        gallery: {
+            enabled: true,
+            navigateByImgClick: true,
+            preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
+        },
+        image: {
+            tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+            titleSrc: function (item) {
+                return item.el.attr('title');
+            }
+        }
+    });
+});
